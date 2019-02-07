d629b0cea101921d83872eaa9574a6797871cc4e
Philip Loa <boyet007@gmail.com>
2019-02-07 15:05:52 +0700

    create static web

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: "Storyblok + Nuxt = <3",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content:'An awesome blog about tech stuff'} 
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Merienda:400,700'}
     ]
   },
 
---
+++ pages/_postId/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <section id="about-page">
+        <h1>Hi, that's us!</h1>
+        <p>We're a bunch of highliy motivated and taltented person bla bla bla..</p>
+    </section>
+</template>
