a5bda9ead3502cdee36714c1b889c8c74130459a
Philip Loa <boyet007@gmail.com>
2019-02-07 16:10:17 +0700

    Nuxt.js & Storyblok - building a complete blog

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,45 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtjs_storybooks
-      </h1>
-      <h2 class="subtitle">
-        My neat Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+    <PostPreview v-for="post in posts" 
+    :key="post.id" 
+    :title="post.title"
+    :excerpt="post.previewText"
+    :thumbnailUrl="post.thumbnailUrl"
+    :id="post.id" />
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import PostPreview from '@/components/Blog/PostPreview'
 export default {
   components: {
-    Logo
+    PostPreview
+  },
+  data() {
+    return {
+      posts: [{
+        title: 'A New begining',
+        previewText: 'This will awesome, don\'t miss it!',
+        thumbnailUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/0b/c5/5e/da/pizza-hut.jpg',
+        id: 'a-new-beginning'
+      },
+      {
+        title: 'A second begining',
+        previewText: 'This will second, don\'t miss it!',
+        thumbnailUrl: 'https://2.bp.blogspot.com/-6rl8LGKtvqw/VqQeN3WUvfI/AAAAAAAAAgM/0QCpKnHEOkw/s320/Resep-Cara-Membuat-Sop-Iga-Sapi-Bening-1.jpg',
+        id: 'a-second-beginning'
+      }]
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style scoped>
+  #posts {
+    padding-top: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 </style>
